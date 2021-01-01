d537a8852f79f75829681d93bf270d7e03fdd7cf
AxelGarciaTello <gata21022000@hotmail.com>
2021-01-01 17:27:32 -0600

    Ingreso de administradores al menu principal

---
+++ Bibliotecas/php/IngresoAdministrador.php
@@ -0,0 +1,35 @@
+<?php
+  $Usuario = $_GET['Usuario'];
+  $Contrasenia = $_GET['Contrasenia'];
+
+  session_start();
+
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+
+  //Iniciar conexion
+  $conn = mysqli_connect($servername, $username, $password);
+  //Checar conexion
+  if(!$conn){
+    die("Conexion fallida: " . mysqli_connect_error());
+  }
+
+  $sql = "use `WEB`";
+  $result = mysqli_query($conn, $sql);
+
+  $sql = "SELECT * FROM `administrador` WHERE Usuario='".$Usuario."' AND Contrasenia=sha2('".$Contrasenia."', 224)";
+
+  $result = mysqli_query($conn, $sql);
+
+  if (mysqli_num_rows($result) > 0) {
+    $row = mysqli_fetch_assoc($result);
+    $_SESSION["Usuario"] = $row["Usuario"];
+    $_SESSION["Contrasenia"] = $row["Contrasenia"];
+    echo "Aceptado";
+  }
+  else{
+    echo "Usuario o contraseña incorrectos.";
+  }
+
+?>
