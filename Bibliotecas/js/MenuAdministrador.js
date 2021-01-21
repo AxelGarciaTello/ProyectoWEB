ac8d53269199e57f55ccb7a3de1c974e3c1ce436
AxelGarciaTello <gata21022000@hotmail.com>
2021-01-20 22:41:53 -0600

    Redireccionamiento al nuevo menu

--- Bibliotecas/js/MenuAdministrador.js
+++ Bibliotecas/js/MenuAdministrador.js
@@ -1,26 +1,26 @@
 function Alumnos(){
-  document.getElementById("pantalla").innerHTML = "<iframe width=\"100%\" height=\"380%\" src=\"InformacionAlumnos.html\" scrolling=\"yes\" frameborder=\"yes\" ></iframe>";
+  document.getElementById("pantalla").innerHTML = "<iframe width=\"100%\" height=\"380%\" src=\"../../Administrador/InformacionAlumnos.html\" scrolling=\"yes\" frameborder=\"yes\" ></iframe>";
   document.getElementById("alumnos").classList.add('active');
   document.getElementById("calificar").classList.remove('active');
   document.getElementById("reportes").classList.remove('active');
   document.getElementById("horarios").classList.remove('active');
 }
 function Calificar(){
-  document.getElementById("pantalla").innerHTML = "<iframe width=\"100%\" height=\"380%\" src=\"CalificacionAlumnos.html\" scrolling=\"yes\" frameborder=\"yes\" ></iframe>";
+  document.getElementById("pantalla").innerHTML = "<iframe width=\"100%\" height=\"380%\" src=\"../../Administrador/CalificacionAlumnos.html\" scrolling=\"yes\" frameborder=\"yes\" ></iframe>";
   document.getElementById("alumnos").classList.remove('active');
   document.getElementById("calificar").classList.add('active');
   document.getElementById("reportes").classList.remove('active');
   document.getElementById("horarios").classList.remove('active');
 }
 function Reportes(){
-  document.getElementById("pantalla").innerHTML = "<iframe width=\"100%\" height=\"380%\" src=\"../Bibliotecas/php/MostrarGraficos.php\" scrolling=\"yes\" frameborder=\"yes\" ></iframe>";
+  document.getElementById("pantalla").innerHTML = "<iframe width=\"100%\" height=\"380%\" src=\"MostrarGraficos.php\" scrolling=\"yes\" frameborder=\"yes\" ></iframe>";
   document.getElementById("alumnos").classList.remove('active');
   document.getElementById("calificar").classList.remove('active');
   document.getElementById("reportes").classList.add('active');
   document.getElementById("horarios").classList.remove('active');
 }
 function Horarios(){
-  document.getElementById("pantalla").innerHTML =  "<iframe width=\"100%\" height=\"380%\" src=\"InformacionHorarios.html\" scrolling=\"yes\" frameborder=\"yes\" ></iframe>";
+  document.getElementById("pantalla").innerHTML =  "<iframe width=\"100%\" height=\"380%\" src=\"../../Administrador/InformacionHorarios.html\" scrolling=\"yes\" frameborder=\"yes\" ></iframe>";
   document.getElementById("alumnos").classList.remove('active');
   document.getElementById("calificar").classList.remove('active');
   document.getElementById("reportes").classList.remove('active');
